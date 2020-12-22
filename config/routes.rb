5afdfed25bdb53163802bcf25cfd679a5a525d5a
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-23 00:05:56 +0300

    Update dashboard controller, routes.rb file,  sidebar partial view; add  profile route, profile view and profile action method inside the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'public#main'
-
+  
+  get '/profile/:id' => 'dashboard#profile', as: :profile
   
 end
