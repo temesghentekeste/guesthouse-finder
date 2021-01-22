011d42e4e6a3e493559ba1353c19d7468e77cdf2
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-01-22 23:11:48 +0300

    Refactor code; change modal to partial; setup routes; add action menthod inside properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -66,6 +66,14 @@ class PropertiesController < ApplicationController
     end
   end
 
+  def email_agent 
+    # trigger email send
+
+
+    # response to script
+    format.json { head :no_content}
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_property
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   resources :properties
   get 'public/main'
   get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'public#main'
   
   get '/profile/:id' => 'dashboard#profile', as: :profile
+  post 'agent/message' => 'properties#email_agent', as: :email_agent
   
 end
